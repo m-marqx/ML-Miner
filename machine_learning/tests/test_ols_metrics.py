e44c23e15f48ed2c1e9881006dcb27d429b27e5f
Mateus Marques <124513922+m-marqx@users.noreply.github.com>
2024-10-24 19:10:23 -0500

    test(calculate_r2): add unittest

---
+++ machine_learning/tests/test_ols_metrics.py
@@ -0,0 +1,12 @@
+import unittest
+import pandas as pd
+import numpy as np
+from machine_learning.ols_metrics import calculate_coef, calculate_r2
+
+class TestCalculateOLS(unittest.TestCase):
+    def setUp(self):
+        rng = np.random.default_rng(33)
+        self.dataset = pd.Series(rng.integers(0, 100, 100))
+
+    def test_calculate_R2(self):
+        self.assertEqual(calculate_r2(self.dataset), 0.0021)
