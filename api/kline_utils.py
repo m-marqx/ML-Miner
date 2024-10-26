558352feaee88a9515dc879d9c8ace556ed5c1fc
Mateus Marques <124513922+m-marqx@users.noreply.github.com>
2024-10-25 22:23:39 -0500

    refactor(KlineTimes): transform `default_intervals` to be a class attribute

--- api/kline_utils.py
+++ api/kline_utils.py
@@ -23,8 +23,6 @@ class KlineTimes:
 
     Methods
     -------
-    default_intervals()
-        Returns the list of default intervals.
     calculate_max_multiplier(max_candle_limit: int = 1500)
         Calculate the maximum multiplier based on the interval.
     get_end_times(start_time=1597118400000, max_candle_limit=1500)
@@ -46,19 +44,7 @@ class KlineTimes:
         """
         self.symbol = symbol
         self.interval = interval
-
-    @property
-    def default_intervals(self):
-        """
-        Returns the list of default intervals.
-
-        Returns
-        -------
-        list of str
-            The list of default intervals.
-
-        """
-        return [
+        self.default_intervals = [
             "1s",
             "1m",
             "5m",
