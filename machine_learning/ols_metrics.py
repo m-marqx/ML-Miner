8345b1190aa13ba31b4803f994f1414b5f45709f
Mateus Marques <124513922+m-marqx@users.noreply.github.com>
2024-10-30 22:30:15 -0500

    fix(calculate_r2): fix R² calculation

--- machine_learning/ols_metrics.py
+++ machine_learning/ols_metrics.py
@@ -17,11 +17,11 @@ def calculate_r2(data: pd.Series, precision: int = 6) -> float:
         The R-squared value.
 
     """
-    x = data.reset_index().index
-    x = sm.add_constant(x)
-    fit_results = sm.OLS(data, x, missing="drop").fit()
+    x = data.index
+    x = sm.add_constant(x.astype("int64") / 10**9)
+    fit_results = sm.OLS(data.astype("float64"), x, missing="drop").fit()
 
-    return round(fit_results.rsquared, precision)
+    return float(f"{fit_results.rsquared:.{precision}g}")
 
 
 def calculate_coef(data: pd.Series, precision: int = 7) -> float:
--- machine_learning/tests/test_ols_metrics.py
+++ machine_learning/tests/test_ols_metrics.py
@@ -9,7 +9,7 @@ class TestCalculateOLS(unittest.TestCase):
         self.dataset = pd.Series(rng.integers(0, 100, 100))
 
     def test_calculate_R2(self):
-        self.assertEqual(calculate_r2(self.dataset), 0.0021)
+        self.assertEqual(calculate_r2(self.dataset), 0.00209958)
 
     def test_calculate_coef(self):
         self.assertEqual(calculate_coef(self.dataset), -0.0438764)
