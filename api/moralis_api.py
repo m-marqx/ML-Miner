b79dec875af6330d0f27ffb29b5835fc12c03e23
Mateus Marques <124513922+m-marqx@users.noreply.github.com>
2024-11-17 21:28:16 -0500

    feat(MoralisAPI): add get_transactions method

--- api/moralis_api.py
+++ api/moralis_api.py
@@ -111,3 +111,46 @@ class MoralisAPI:
 
         raise ValueError("data has less than 2 elements")
 
+    def get_transactions(self, wallet: str) -> list:
+        """
+        Retrieves all swaps for a given wallet address.
+
+        Parameters
+        ----------
+        wallet : str
+            The wallet address to retrieve swaps for.
+        """
+        self.logger.info("Retrieving transactions for wallet: %s", wallet)
+
+        params = {
+            "chain": "polygon",
+            "order": "DESC",
+            "address": wallet,
+        }
+
+        txn_infos = evm_api.wallets.get_wallet_history(
+            api_key=self.api_key,
+            params=params,
+        )["result"]
+
+        swaps = []
+
+        excluded_categories = [
+            "contract interaction",
+            "token receive",
+            "airdrop",
+            "receive",
+            "approve",
+        ]
+
+        for txn in txn_infos:
+            is_not_spam = not txn["possible_spam"]
+            in_excluded_categories = txn["category"] in excluded_categories
+
+            if is_not_spam and not in_excluded_categories:
+                swaps.append(txn)
+
+        self.logger.info("Retrieved %d transactions", len(swaps))
+
+        return swaps
+
