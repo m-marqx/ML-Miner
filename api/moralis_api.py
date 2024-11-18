992acc5eb933334d2778577c84bf13e352a57f34
Mateus Marques <124513922+m-marqx@users.noreply.github.com>
2024-11-17 21:27:44 -0500

    feat: add MoralisAPI class

---
+++ api/moralis_api.py
@@ -0,0 +1,68 @@
+import logging
+
+import pandas as pd
+import numpy as np
+
+from moralis import evm_api
+
+
+class MoralisAPI:
+    """
+    A class to interact with the Moralis API for retrieving transaction
+    data.
+
+    Parameters
+    ----------
+    verbose : bool
+        If True, sets the logger to INFO level.
+    api_key : str
+        The API key for the Moralis API.
+
+    Attributes
+    ----------
+    logger : logging.Logger
+        Logger instance for logging information.
+    api_key : str
+        The API key for the Moralis API.
+
+    Methods
+    -------
+    process_transaction_data(data)
+        Processes transaction data for a given transaction.
+    get_swaps(wallet)
+        Retrieves all swaps for a given wallet address.
+    get_swaps_data(swaps)
+        Retrieves all swaps data for a given wallet address.
+    get_account_swaps(wallet)
+        Retrieves all swaps for a given wallet address.
+    """
+
+    def __init__(self, verbose: bool, api_key: str):
+        """
+        Initialize the MoralisAPI object.
+
+        Parameters
+        ----------
+        verbose : bool
+            If True, sets the logger to INFO level.
+        api_key : str
+            The API key for the Moralis API.
+        """
+        self.api_key = api_key
+
+        self.logger = logging.getLogger("moralis_API")
+        formatter = logging.Formatter(
+            "%(levelname)s %(asctime)s: %(message)s", datefmt="%H:%M:%S"
+        )
+
+        handler = logging.StreamHandler()
+        handler.setFormatter(formatter)
+
+        if self.logger.hasHandlers():
+            self.logger.handlers.clear()
+
+        self.logger.addHandler(handler)
+        self.logger.propagate = False
+
+        if verbose:
+            self.logger.setLevel(logging.INFO)
