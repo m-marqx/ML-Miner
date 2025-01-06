3ba668cce9eba19967e5e7695d1b955cd9887539
Mateus Marques <124513922+m-marqx@users.noreply.github.com>
2025-01-05 20:48:29 -0500

    feat: add `ModelMetrics` class

---
+++ machine_learning/model_metrics.py
@@ -0,0 +1,61 @@
+import warnings
+
+import pandas as pd
+
+from machine_learning.ml_utils import DataHandler
+from machine_learning.ols_metrics import OLSMetrics
+from custom_exceptions.invalid_arguments import InvalidArgumentError
+from utils import Statistics
+
+
+class ModelMetrics:
+    """
+    A class to calculate and store various model performance metrics.
+
+    The ModelMetrics class handles calculation of performance metrics
+    for both test and validation periods, including returns, drawdowns,
+    statistical ratios and operation counts.
+    """
+
+    def __init__(
+        self,
+        train_in_middle: bool,
+        index_splits: dict[str, pd.Interval],
+        results: pd.DataFrame,
+        test_index: int,
+    ):
+        """
+        Initialize the ModelMetrics object.
+
+        Parameters
+        ----------
+        train_in_middle : bool
+            Whether the training period is in the middle of the dataset.
+        index_splits : dict[str, pd.Interval]
+            Dictionary containing interval splits for
+            train/test/validation.
+        results : pd.DataFrame
+            DataFrame containing model prediction results.
+        test_index : int
+            Length of the test period in days.
+        """
+        self.train_in_middle = train_in_middle
+        self.test_index = test_index
+
+        self.test_periods, self.val_periods = self.get_periods(index_splits)
+
+        self.results = results
+
+        if self.train_in_middle:
+            self.results_test = results.loc[
+                self.test_periods[0] : self.test_periods[1]
+            ]
+        else:
+            self.results_test = results.loc[
+                self.test_periods[0] : self.test_periods[1]
+            ]
+
+        self.results_val = results.loc[
+            self.val_periods[0] : self.val_periods[1]
+        ]
+
