218a4232211ee25e01ee9e11491d3120fd6f50dd
Mateus Marques <124513922+m-marqx@users.noreply.github.com>
2025-04-09 23:15:58 -0500

    feat(ModelPipeline): add `create_model` method

--- back-end/machine_learning/ml_pipeline.py
+++ back-end/machine_learning/ml_pipeline.py
@@ -22,3 +22,50 @@ class ModelPipeline:
         self.model_df["Target_bin"] = self.model_df["Target_bin"].replace(
             {0: -1}
         )
+
+    def create_model(self, configs_dataset, dataframe):
+        """
+        Create a machine learning model based on the provided 
+        configuration parameters.
+
+        Parameters
+        ----------
+        configs_dataset : pandas.Series
+            Series containing configuration parameters including 
+            hyperparameters,
+            feature parameters, test_index, train_in_middle, side, 
+            max_trades, and off_days.
+        dataframe : pandas.DataFrame
+            The input dataframe containing the features and target 
+            variables.
+
+        Returns
+        -------
+        pandas.DataFrame
+            DataFrame containing model results including predictions and
+            performance metrics.
+        """
+        hyperparams = literal_eval(configs_dataset.loc["hyperparameters"])[0]
+        hyperparams["iterations"] = 1000
+
+        feat_params = literal_eval(configs_dataset.loc["feat_parameters"])[0]
+        test_index = int(configs_dataset.loc["test_index"])
+        train_in_mid = configs_dataset.loc["train_in_middle"]
+        side = int(configs_dataset.loc["side"])
+        max_trades = int(configs_dataset.loc["max_trades"])
+        off_days = int(configs_dataset.loc["off_days"])
+
+        model_results, _, _, _ = model_creation(
+            feat_params,
+            hyperparams,
+            test_index,
+            dataframe,
+            dev=False,
+            train_in_middle=train_in_mid,
+            cutoff_point=5,
+            side=side,
+            max_trades=max_trades,
+            off_days=off_days,
+        )
+
+        return model_results
