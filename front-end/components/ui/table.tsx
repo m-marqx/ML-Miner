4008853277d97c6844e38c1097f1871a81b4c0c4
Mateus Marques <124513922+m-marqx@users.noreply.github.com>
2025-05-26 21:40:05 -0500

    refactor(TableRow): update hover and background color

--- front-end/components/ui/table.tsx
+++ front-end/components/ui/table.tsx
@@ -46,7 +46,7 @@ function TableRow({ className, ...props }: React.ComponentProps<"tr">) {
     <tr
       data-slot="table-row"
       className={cn(
-        "hover:bg-muted/50 data-[state=selected]:bg-muted border-b transition-colors",
+        "hover:bg-zinc-800 data-[state=selected]:bg-muted border-b border-zinc-800 transition-colors",
         className
       )}
       {...props}
