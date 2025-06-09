8e8dd46e1da9f310b6593f51a9c247adab5ecc5d
Mateus Marques <124513922+m-marqx@users.noreply.github.com>
2025-06-08 21:09:52 -0500

    refactor(Home): remove unused wallet address logic

--- front-end/src/app/swap/page.tsx
+++ front-end/src/app/swap/page.tsx
@@ -3,47 +3,26 @@
 import React from "react";
 
 import { SwapWidget } from "@/src/components/SwapWidget/swapWidget";
-import { useAccount } from 'wagmi'
 import Squares from "@/blocks/Backgrounds/Squares/Squares"
 import { AppSidebar } from "@/src/components/Sidebar/sidebar";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar"
 
 export default function Home() {
-    const { address: walletAddress } = useAccount();
-
-    if (!walletAddress) {
-        return (
-            <div className="relative w-full h-full">
-                <Squares
-                    speed={0}
-                    squareSize={56}
-                    borderColor='#71717a40'
-                />
-                <div className="grid grid-cols-[1fr_2fr] h-[calc(100%-2rem)] gap-[3.4svw] m-[3svh_2.7svw] justify-center relative z-10" >
-                    <div></div>
-                    <div className="w-140">
-                        <SwapWidget />
-                    </div>
-                </div>
-            </div>
-        )
-    }
-
     return (
         <SidebarProvider open={true} defaultOpen={true}>
             <AppSidebar />
-        <SidebarInset className="w-0">
-        <div className="relative w-full h-full flex flex-col justify-center items-center">
-            <Squares
-                speed={0}
-                squareSize={56}
-                borderColor='#71717a40'
-            />
-            <div className="w-fit h-fit items-center flex justify-center relative z-10" >
-                <SwapWidget />
-            </div>
+            <SidebarInset className="w-0">
+                <div className="relative w-full h-full flex flex-col justify-center items-center">
+                    <Squares
+                        speed={0}
+                        squareSize={56}
+                        borderColor='#71717a40'
+                    />
+                    <div className="w-fit h-fit items-center flex justify-center relative z-10" >
+                        <SwapWidget />
+                    </div>
                 </div>
-        </SidebarInset>
-      </SidebarProvider >
+            </SidebarInset>
+        </SidebarProvider >
     );
 }
