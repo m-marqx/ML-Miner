1447f2939238a9986d62dc170c9820bb9fa51199
Mateus Marques <124513922+m-marqx@users.noreply.github.com>
2025-08-15 22:40:51 -0500

    feat: add `ModelStacker` class

---
+++ machine_learning/ml_stacker.py
@@ -0,0 +1,24 @@
+class ModelStacker:
+    def __init__(
+        self,
+        dataset,
+        onchain_data,
+        test_index,
+        train_in_middle,
+        max_trades,
+        off_days,
+        side,
+        cutoff_point,
+        best_model_dict: dict,
+    ):
+        self.dataset = dataset
+        self.onchain_data = onchain_data
+        self.test_index = test_index
+        self.train_in_middle = train_in_middle
+        self.target_series = self.dataset["Target"]
+        self.y_true = self.dataset['Target_bin']
+        self.max_trades = max_trades
+        self.off_days = off_days
+        self.side = side
+        self.cutoff_point = cutoff_point
+        self.best_model_dict = best_model_dict
