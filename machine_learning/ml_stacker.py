66a63c72ad2fec8c916555b37ea813187997e6a9
Mateus Marques <124513922+m-marqx@users.noreply.github.com>
2025-08-22 22:30:18 -0500

    feat(MLStacker): add `model_creator` attribute

--- machine_learning/ml_stacker.py
+++ machine_learning/ml_stacker.py
@@ -1,3 +1,12 @@
+import time
+
+import pandas as pd
+
+from machine_learning.model_creator import ModelCreator
+from machine_learning.model_metrics import ModelMetrics
+from machine_learning.ml_utils import calculate_returns
+
+
 class MLStacker:
     def __init__(
         self,
@@ -24,3 +33,7 @@ class MLStacker:
         self.cutoff_point = cutoff_point
         self.best_model_dict = best_model_dict
         self.verbose = verbose
+        self.model_creator = ModelCreator(
+            dataset, onchain_data, test_index, verbose, train_in_middle
+        )
+
